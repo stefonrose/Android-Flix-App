7842f74fa860ffe744ef897ebdfcb6c07159b7cf
stefonrose <stephonfonrose@gmail.com>
2021-02-09 22:26:27 -0500

    finished required stories for part 2

--- app/src/main/java/com/sfonrose/flixter/models/Movie.java
+++ app/src/main/java/com/sfonrose/flixter/models/Movie.java
@@ -3,19 +3,27 @@ package com.sfonrose.flixter.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String posterPath;
     String title;
     String overview;
+    double rating;
+    int id;
+
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        id = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -36,5 +44,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getId() {
+        return id;
     }
 }
